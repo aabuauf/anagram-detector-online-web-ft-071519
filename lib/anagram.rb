477d17516b6e80b5f52cb0335faf4ff0fbca5518
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-24 19:31:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,17 @@
 # Your code goes here!
 class Anagram 
+  def initialize(word)
+    @woed = word
+  end
   
+  def match(sentence)
+    wordArray = @word.split("").sort
+    sentenceArray = sentence.split(" ")
+    sentenceArray.each do|sentenceWord| 
+      if sentenceWord.split("").sort == wordArray
+        resultArray << @word
+      end
+    end
+    return resultArray
+  end
 end
